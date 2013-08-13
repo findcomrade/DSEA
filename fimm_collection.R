70f28bd2bdd71c7052262e4b64a0c072c9412c36
findcomrade <piter.dmitry@gmail.com>
2013-08-13 15:25:39 +0300

    fimm collection analysis for ATC drugs

--- fimm_collection.R
+++ fimm_collection.R
@@ -1,5 +1,13 @@
+#
+# This is to sort out initial (raw) data
+# and generate comprehensive FIMM Drug Dictionary
+# along with Annotations for those drugs;
+# HTML Summary included.
+#
+#######################################################
+
 setwd("/home/comrade/Ubuntu One/DSEA/r-code")
-#load("../datasets/merged_chembl.RData")
+# load("../datasets/merged_chembl.RData")
 
 library(vcd)
 library(grid)
@@ -21,32 +29,36 @@ remove(merged_chembl_ATC_SCID)  # cleanup
 # ============================= #
 fimmcollection.section.REPORT <- newSection( "Fimm Collection Annotations" )
 
-# Extract relevant information from 'ChEMBL db'
-# to annotate drugs in FIMM data base.
 fimm.drug.count <- length( unique(fimm.DICT$FIMM.batch.ID) )
-drop <- which(is.na(fimm.DICT$pubchem_CIDs))
-fimm.DICT <- fimm.DICT[-drop,]  # drop drugs without pubchem_CIDs in FIMM Dctionary
+# Drop drugs without pubchem_CIDs in FIMM Dctionary
+drop <- which( is.na(fimm.DICT$pubchem_CIDs) )
+fimm.DICT <- fimm.DICT[-drop,]
+
+# Count unique PUBCHEM IDs
 fimm.drug.CID <- length( unique(fimm.DICT$pubchem_CIDs) )
-fimm.drug.CHE <- length( unique(fimm.DICT$CHEMBL_ID) )
-fimm.SUMMARY <- data.frame(FIMM.Batch=fimm.drug.count, PubChem.CIDs=fimm.drug.CID, Chembl.IDs=fimm.drug.CHE)
-remove(fimm.drug.CID, fimm.drug.count, fimm.drug.CHE)
+fimm.SUMMARY <- data.frame(FIMM.Batch=fimm.drug.count, PubChem.CIDs=fimm.drug.CID)
+remove(fimm.drug.CID, fimm.drug.count)  # clean up
 
 # Build annotation table for FIMM Collection
 drop <- atc.ANNO[,"Pubchem_CID"] %in% unique(fimm.DICT[,"pubchem_CIDs"]) 
 fimm.ANNO <- atc.ANNO[drop,-c(19,20)]
 
-# drop rows without annotations
-drop <- which(is.na(fimm.ANNO$Level1))
+# Drop Rows (compounds) without Annotations
+drop <- which( is.na(fimm.ANNO$Level1) )
 missing.ANNO <- fimm.ANNO[drop,]
 fimm.ANNO <- fimm.ANNO[-drop,]
 fimm.ANNO <- unique(fimm.ANNO)  # remove duplicated rows
-fimm.SUMMARY$PubChem.Annotated <- length(fimm.ANNO$Pubchem_CID)
+fimm.SUMMARY$Annotations <- length(fimm.ANNO$Pubchem_CID)
+fimm.SUMMARY$PubChem.Annotated <- length( unique(fimm.ANNO[,"Pubchem_CID"]) )
+# fimm.SUMMARY$Missing.Annotat <- length( unique(missing.ANNO[,"Pubchem_CID"]) )
 
 fimmcollection.section.REPORT <- addTo( fimmcollection.section.REPORT, newTable( fimm.SUMMARY, "FIMM Collection Summary" ) )
 fimmcollection.section.REPORT <- addTo( fimmcollection.section.REPORT, newTable( fimm.DICT[1:7,], "FIMM Dictionary" ) )
 fimmcollection.section.REPORT <- addTo( fimmcollection.section.REPORT, newTable( fimm.ANNO[1:3,c(1,8,9,10,11)], "FIMM Annotations" ) )
 remove(atc.ANNO, drop)
 
+# Save R Objects to a file
+save(fimm.ANNO, missing.ANNO, fimm.DICT, fimm.SUMMARY, file = "FimmATCDrugAnnotations.RData")
 
 REPORT <- addTo( REPORT, fimmcollection.section.REPORT );
 writeReport( REPORT, filename="../reports/FIMMCollection/REPORT" )
